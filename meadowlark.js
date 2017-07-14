94908e2ac3246dddcdfd1f4dad20fd479328a2e1
mega <mega@esls.io>
2017-07-14 11:12:30 +0900

    ch03 finish

--- meadowlark.js
+++ meadowlark.js
@@ -12,11 +12,20 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+var fortunes = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple.",
+];
+
 app.get('/', function(req, res) {
 	res.render('home');
 });
 app.get('/about', function(req,res){
-	res.render('about');
+	var rf = fortunes[Math.floor(Math.random()*fortunes.length)];
+	res.render('about',{fortune:rf});
 });
 
 app.use(function(req, res, next){
