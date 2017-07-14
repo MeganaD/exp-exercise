d84f239034fc774c13cde83d8ffa46336bbe25a9
mega <mega@esls.io>
2017-07-14 11:21:10 +0900

    포춘쿠기를 기능 모듈로 분리

--- meadowlark.js
+++ meadowlark.js
@@ -1,8 +1,8 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
-// set up handlebars view engine
 var handlebars = require('express-handlebars')
 	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
@@ -12,20 +12,11 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
-
 app.get('/', function(req, res) {
 	res.render('home');
 });
 app.get('/about', function(req,res){
-	var rf = fortunes[Math.floor(Math.random()*fortunes.length)];
-	res.render('about',{fortune:rf});
+		res.render('about',{fortune:fortune.getFortune()});
 });
 
 app.use(function(req, res, next){
